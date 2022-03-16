d260b729f1b070fa37168274cd64a2336de3eff2
kemalhibatullah <mohkemal@mail.ugm.ac.id>
2022-03-17 05:53:26 +0700

    Splash Screen

---
+++ lib/theme.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+
+Color blackColor = Color(0xff000000);
+Color blueColor = Color(0xff4361FE);
+Color grayColor = Color(0xff686868);
+Color whiteColor = Color(0xffffffff);
+
+TextStyle titleTextStyle = GoogleFonts.inter(
+  color: blackColor,
+  fontWeight: FontWeight.w600,
+  fontSize: 20,
+);
+
+TextStyle descTextStyle = GoogleFonts.inter(
+  color: grayColor,
+  fontWeight: FontWeight.w300,
+  fontSize: 14,
+);
+
+TextStyle buttonBlueTextStyle = GoogleFonts.inter(
+  color: whiteColor,
+  fontWeight: FontWeight.w600,
+  fontSize: 14,
+);
+
+TextStyle buttonBorderTextStyle = GoogleFonts.inter(
+  color: blueColor,
+  fontWeight: FontWeight.w600,
+  fontSize: 14,
+);
